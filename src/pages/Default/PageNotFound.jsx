c855ca6d8c873d22d2e87bd9426e9fa6b652a4cf
Ng.DucHuy <duchuytv2102@gmail.com>
2024-09-12 23:16:14 +0700

    update page not found

--- src/pages/Default/PageNotFound.jsx
+++ src/pages/Default/PageNotFound.jsx
@@ -1,18 +1,27 @@
+import { Button } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
     return (
-        <div className='h-screen flex flex-col items-center justify-center'>
-            <p className='text-[10rem] font-bold mb-3 text-transparent bg-gradient-to-r from-sky-500 to-red-500 bg-clip-text'>
-                Lỗi!
-            </p>
-            <p className='uppercase text-xl font-bold mb-3'>404 - Không tìm thấy trang</p>
-            <p className='mb-3 text-lg'>Trang bạn đang tìm kiếm không tồn tại hoặc đã bị xóa.</p>
-            <Link
-                to='/'
-                className='transition ease-in-out duration-300 hover:scale-110 hover:bg-blue-500 uppercase text-lg px-7 py-4 text-center rounded-xl bg-black text-white hover:border-none'
+        <div
+            className='h-screen max-w-md w-full mx-auto 
+            flex flex-col items-center justify-center gap-y-1'
+        >
+            <span
+                className='text-[10rem] font-bold text-transparent 
+            bg-gradient-to-r from-sky-500 to-red-500 bg-clip-text'
             >
-                trở lại trang chủ
+                Lỗi!
+            </span>
+            <span className='uppercase text-xl font-bold'>Không tìm thấy trang</span>
+            <span className='text-lg'>Trang bạn đang tìm kiếm không tồn tại hoặc đã bị xóa.</span>
+            <Link className='w-full py-1' to={'/'}>
+                <Button
+                    className='w-full transition ease-in-out duration-300 hover:scale-110'
+                    outline
+                >
+                    Trở lại trang chủ
+                </Button>
             </Link>
         </div>
     );
